1282f65ae7b6b8ad75897fe5440ff064d620b1c1
Richa-1410 <richaverma6161@gmail.com>
2026-04-17 13:45:02 +0530

    javafunc

---
+++ javascript/functions/app.js
@@ -0,0 +1,29 @@
+//array
+let numbers= [1,2,3,4,5];
+console.log(numbers[0]);
+console.log(numbers[2]);
+console.log(numbers[1]);
+//mix array
+let mix = [1,"hello",{name:"smita"}];
+console.log(mix[0]);
+console.log(mix[1]);
+console.log(mix[2]);
+console.log(mix.length);
+//functions
+function greet(){
+    console.log("hello world");
+}
+greet();
+//normal function
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(2,3));
+//function expression(create funcn through variable)
+const divide = function(a,b){
+    return a/b;
+};
+console.log(divide(6,3));
+//arrow funcn with no parameters
+let hi =()=> console.log("hi"); //always through funcn expression
+hi();
